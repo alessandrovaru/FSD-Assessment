72fd8ffb7b6b4aba65199f622357bc2d73a11217
Alessandro <alessandrovaru@gmail.com>
2022-02-01 17:23:12 -0400

    Assessment finished

--- script.js
+++ script.js
@@ -11,11 +11,29 @@ const Products = {
    * Sets up the query string for the GraphQL request
    * @returns {String} A GraphQL query string
    */
-  query: () => `
-
-    Add your GraphQL query here
-    
-  `,
+  query: () => `{
+    products(first:3) { 
+      edges {
+        node {
+          id
+          title
+          tags
+          images (first:1) {
+            edges {
+              node {
+                originalSrc
+              }
+            }
+          },
+          priceRange {
+            maxVariantPrice {
+              amount
+            }
+          }
+        }
+      }
+    }
+  }`,
 
   /**
    * Fetches the products via GraphQL then runs the display function
@@ -24,9 +42,9 @@ const Products = {
     const productsResponse = await fetch(Products.state.storeUrl, {
       method: "POST",
       headers: {
-
-        // Set up the request headers here
-
+        "Content-Type": Products.state.contentType,
+        Accept: Products.state.accept,
+        "X-Shopify-Storefront-Access-Token": Products.state.accessToken,
       }, 
       body: JSON.stringify({
         query: Products.query()
@@ -42,8 +60,31 @@ const Products = {
    */
   displayProducts: productsJson => {
 
-    // Render the products here
+    const container = document.getElementById("cards-container");
+    console.log(container);
+    let products = productsJson.data.products.edges;
+    console.log(products);
 
+    products.map(product => {
+      const { node } = product;
+      const { title, tags, images, priceRange} = node;
+      const cards = `
+      <div class="card">
+        <span class="gender">${tags}</span>
+        <div style="background-image:url(${images.edges[0].node.originalSrc});" class="card-img">
+        </div>
+        <h2>${title}</h2>
+        <p>$${priceRange.maxVariantPrice.amount}</p>
+        <button type="button" class="btn btn-warning">
+        Shop Now 
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+          <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0v-6z"/>
+          </svg>
+        </button>
+      </div>
+    `;
+      container.innerHTML += cards;
+    })
   },
 
   /**
@@ -51,7 +92,11 @@ const Products = {
    */
   initialize: () => {
 
-    // Add the click handler here
+    addEventListener("click", event => {
+      if (event.target.id === "fetch-products") {
+        Products.handleFetch();
+      }
+    });
 
   }
 
